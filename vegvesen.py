b64d347a5466f39f9f8965a8a1beec1aacb02f9d
Knut Helge Pedersen <p72@storebrand.no>
2016-04-05 11:45:47 +0200

    Insert XML as JSON data in mongodb

--- vegvesen.py
+++ vegvesen.py
@@ -1,9 +1,22 @@
 import os
 import requests
+import xmltodict
+import pymongo
 
 url = 'https://www.vegvesen.no/ws/no/vegvesen/veg/trafikkpublikasjon/reisetid/1/GetTravelTimeData'
 username = os.environ.get('VEGVESEN_USERNAME')
 password = os.environ.get('VEGVESEN_PASSWORD')
 r = requests.get(url, auth=(username, password))
 
-print (r.text)
+xml = r.text
+
+doc = xmltodict.parse(xml)
+
+print(doc['d2LogicalModel']['exchange']['supplierIdentification']['nationalIdentifier'])
+print(doc['d2LogicalModel']['exchange'])
+
+client = pymongo.MongoClient()
+db = client.test_database
+collection = db.test_collection
+id = collection.insert_one(doc)
+print(id)

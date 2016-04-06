25fd1cf3b3bf001e263f1be0be3ce0508d920d95
Knut Helge Pedersen <p72@storebrand.no>
2016-04-06 16:27:50 +0200

    Return simple JSON

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+import flask
+import os
+import pymongo
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+	mongoUri = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/testdb')
+	print('Using mongodb url=%s', mongoUri)
+	client = pymongo.MongoClient(mongoUri)
+	db = client.get_default_database()
+	collection = db.vegvesen_traveltime
+	ret = {}
+	ret['count'] = str(collection.count())
+	ret['one'] = collection.find().limit(1).sort('{$natural:-1}')[0]['publicationTime']
+	return flask.jsonify(ret)
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
 

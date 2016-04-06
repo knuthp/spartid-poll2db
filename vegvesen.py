2dd325f792fb4bb3833e58c6218e5c04d18a6833
Knut Helge Pedersen <p72@storebrand.no>
2016-04-06 12:22:19 +0200

    Output mongouri

--- vegvesen.py
+++ vegvesen.py
@@ -3,6 +3,25 @@ import requests
 import xmltodict
 import pymongo
 
+def extractElaboratedData( elaboratedData ):
+	""
+	legsDict = {}
+	for leg in elaboratedData:
+		legData = extractBasicData(leg['basicData'])
+		legsDict[legData['id']] = legData
+		# print(legData)
+
+	return legsDict
+
+def extractBasicData( basicData ):
+	""
+	return { 'id' : basicData['pertinentLocation']['predefinedLocationReference']['@id'],
+		 'travelTimeTrendType' : basicData.get('travelTimeTrendType'),
+		 'travelTimeType' : basicData.get('travelTimeType'),
+		 'travelTime' : basicData.get('travelTime', {}).get('duration'),
+		 'freeFlowTravelTime' : basicData['freeFlowTravelTime']['duration'] }
+
+
 url = 'https://www.vegvesen.no/ws/no/vegvesen/veg/trafikkpublikasjon/reisetid/1/GetTravelTimeData'
 username = os.environ.get('VEGVESEN_USERNAME')
 password = os.environ.get('VEGVESEN_PASSWORD')
@@ -16,12 +35,15 @@ payloadPublication = doc['d2LogicalModel']['payloadPublication']
 
 data = {'publicationTime' : payloadPublication['publicationTime'],
 	'publicationCreator' : payloadPublication['publicationCreator']['nationalIdentifier'],
-	'elaboratedData' : [ {'id' : payloadPublication['elaboratedData'][0]['basicData']['pertinentLocation']['predefinedLocationReference']['@id'] }]
+	'legData' : extractElaboratedData(payloadPublication['elaboratedData'])
 }
 
-client = pymongo.MongoClient()
-db = client.test_database
+mongoUri = os.getenv('MONGOLAB_URI', 'mongodb://localhost:27017/testdb')
+print('Using mongodb url=%s', mongoUri)
+client = pymongo.MongoClient(mongoUri)
+db = client.get_default_database()
 collection = db.test_collection
 id = collection.insert_one(data)
-print(id)
-print(collection.find_one({"_id": id.inserted_id}))
+print('Added new document for traveltime, id=%s', str(id.inserted_id))
+
+
